883ec37342869dea8c4156b567a4786184fd8830
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-26 19:39:43 +0100

    feat(model): Add new employee entity

---
+++ ConditionalAutomapperDemo/DTOs/EmployeeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ConditionalAutomapperDemo.DTOs
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ ConditionalAutomapperDemo/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace ConditionalAutomapperDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
